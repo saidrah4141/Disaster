5479e43543fdd3f3d32658c21ff46bd2f59d75f5
Said Rahmani <saidrah4141@gmail.com>
2024-04-27 16:35:38 -0600

    Created Inquirer

---
+++ Inquirer.java
@@ -0,0 +1,28 @@
+package edu.ucalgary.oop;
+
+import java.util.ArrayList;
+
+public class Inquirer {
+
+    private String FIRST_NAME;
+    private String LAST_NAME;
+    private String SERVICES_PHONE;
+    private ArrayList<Interaction> interactionLog;
+
+    public Inquirer(String FIRST_NAME,String LAST_NAME, String SERVICES_PHONE) {
+        this.FIRST_NAME=FIRST_NAME;
+        this.LAST_NAME=LAST_NAME;
+        this.SERVICES_PHONE=SERVICES_PHONE;
+        this.interactionLog = new ArrayList<>();
+
+    }
+
+    public void logInteraction(Interaction interaction) {
+        this.interactionLog.add(interaction);
+    }
+    public String getFirstName() {return this.FIRST_NAME;}
+    public String getLastName() {return this.LAST_NAME;}
+
+    public String getServicesPhone() {return this.SERVICES_PHONE;}
+
+}
