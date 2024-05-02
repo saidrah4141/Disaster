680d1ab03b8383e84a8832dbe2a0b05755ad3400
Said Rahmani <saidrah4141@gmail.com>
2024-05-01 19:41:07 -0600

    DisasterVictimSearch interface

---
+++ DisasterVictimSearch.java
@@ -0,0 +1,10 @@
+
+import java.util.ArrayList;
+import java.util.Set;
+
+public interface DisasterVictimSearch {
+
+    DisasterVictim searchByFirstName(String firstName, Set<DisasterVictim> victimList);
+
+
+}
